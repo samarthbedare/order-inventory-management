f36868f234eeb271c3a97bc7836cbbd6e8cfa284
Samarth Bedare <bedaresamarth@gmail.com>
2026-04-09 10:40:01 +0530

    Initial commit - order-inventory-management project

---
+++ src/main/java/com/project/orderinventorymanagement/OrderInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.orderinventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderInventoryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderInventoryManagementApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/project/orderinventorymanagement/orderservice/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.project.orderinventorymanagement.orderservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping("/hello")
+    public String greet() {
+        return "Hello";
+    }
+}
